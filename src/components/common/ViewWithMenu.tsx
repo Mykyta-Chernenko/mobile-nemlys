ed07c4d36eaf42aba9ddce22472e3cd9531ec508
Nikita Chernenko <nikita.chernenko@arundo.com>
2023-01-04 12:50:09 +1100

    feat: delete account, splash screen

--- src/components/common/ViewWithMenu.tsx
+++ src/components/common/ViewWithMenu.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { BottomSheet, Button, Header, Icon, useTheme } from '@rneui/themed';
-import { TouchableOpacity } from 'react-native';
+import { Alert, TouchableOpacity } from 'react-native';
 import { i18n } from '@app/localization/i18n';
 import { AUTH_STORAGE_KEY, supabase } from '@app/api/initSupabase';
 import { SecondaryButton } from '../buttons/SecondaryButton';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { logErrors } from '@app/utils/errors';
 
 interface Props {
   children: React.ReactNode;
@@ -19,6 +20,35 @@ export const ViewWithMenu = (props: Props) => {
     await supabase.auth.signOut();
     // just to make sure in case something goes wrong
     await AsyncStorage.removeItem(AUTH_STORAGE_KEY);
+  };
+  const deleteAccount = async () => {
+    const res = await supabase.rpc('delete_user');
+    if (res.error) {
+      logErrors(res.error);
+      return;
+    }
+    await logout();
+    await logout();
+  };
+  const deleteAccountAlert = () => {
+    Alert.alert(
+      i18n.t('are_you_sure_you_want_to_delete_account'),
+      undefined,
+      [
+        {
+          text: i18n.t('cancel'),
+          style: 'cancel',
+        },
+        {
+          text: i18n.t('confirm'),
+          onPress: () => void deleteAccount(),
+          style: 'destructive',
+        },
+      ],
+      {
+        cancelable: true,
+      },
+    );
   };
   return (
     <SafeAreaProvider style={{ flexGrow: 1 }}>
@@ -37,17 +67,27 @@ export const ViewWithMenu = (props: Props) => {
         containerStyle={{ padding: '5%' }}
         backdropStyle={{ backgroundColor: 'rgba(255, 255, 255, 0.7)' }}
       >
-        <Button color="error" onPress={() => void logout()} style={{ margin: '1%' }}>
-          {i18n.t('logout')}
-        </Button>
         <SecondaryButton
           type="outline"
           color={theme.colors.black}
           onPress={() => setShowMenu(false)}
-          style={{ margin: '1%' }}
+          style={{ margin: '1%', marginBottom: '5%' }}
         >
           {i18n.t('close')}
         </SecondaryButton>
+        <Button
+          color="warning"
+          onPress={() => {
+            void logout();
+            void logout();
+          }}
+          style={{ margin: '1%' }}
+        >
+          {i18n.t('logout')}
+        </Button>
+        <Button color="error" onPress={() => void deleteAccountAlert()} style={{ margin: '1%' }}>
+          {i18n.t('delete_account')}
+        </Button>
       </BottomSheet>
       {props.children}
     </SafeAreaProvider>
