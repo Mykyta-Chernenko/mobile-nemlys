3835cc77b2eed42effd0e6c75ced71300e2cdc81
Nikita Chernenko <nikita.chernenko@arundo.com>
2023-02-09 20:58:24 +0100

    autorefreshtoke

--- src/api/initSupabase.ts
+++ src/api/initSupabase.ts
@@ -4,6 +4,7 @@ import { SUPABASE_URL, SUPABASE_KEY } from 'react-native-dotenv';
 export const AUTH_STORAGE_KEY = 'key';
 export const supabase = createClient(SUPABASE_URL as string, SUPABASE_KEY as string, {
   auth: {
+    autoRefreshToken: true,
     storage: AsyncStorage as any,
     storageKey: AUTH_STORAGE_KEY,
     detectSessionInUrl: false,
